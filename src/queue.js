91d939ffd5cc4cd0d1b822d199df8caec516f32f
artemmyzrov <a.myzrov@yandex.ru>
2022-10-17 20:51:26 +0400

    completed

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,21 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+ class Queue {
+  queue = null
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value ) {
+    if (this.queue === null) {
+      this.queue = new ListNode(value)
+    } else {
+      let current = this.queue
+
+      while (current.next) {
+        current = current.next
+      }
+      current.next = new ListNode(value)
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.queue
+    this.queue = this.queue.next
+    return current.value
   }
 }
 
